b91b33310176b6e00df003f1d73684aa3f68c18e
James Stephen Ramos <jamesramos1899@gmail.com>
2025-10-27 12:27:29 +0800

    Chapter 10 E-commerce-app

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class);
